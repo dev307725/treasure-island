c8b12fa2ac6d42715652e9ecdcef8ba9bc2dc261
Dylan Villanueva <105649094+dev307725@users.noreply.github.com>
2022-05-20 09:49:45 -0500

    Update main.py

--- main.py
+++ main.py
@@ -25,21 +25,21 @@ print("Your mission is to find the treasure.")
 
 choice1 = input("You're at a crossroad. Where do you want to go? Type \"left\" or \"right\"\n").lower()
 if choice1 == "left":
-  choice2 = input("You come to a lake. There is an island in the middle of the lake. Type \"wait\" to wait for a boat. Type \"swim\" to swim across.\n").lower()
-  if choice2 == "wait":
-    choice3 = input("You arrive to the island unharmed. There is a house with 3 doors. One red, one yellow, and one blue. Which color do you choose?\n").lower()
-    if choice3 == "blue":
-      print("You enter a room of beasts. Game over.")
-    elif choice3 == "red":
-      print("It's a room full of fire. Game over.")
-    elif choice3 == "yellow":
-      print("You found the treasure! You win!")
+    choice2 = input("You come to a lake. There is an island in the middle of the lake. Type \"wait\" to wait for a boat. Type \"swim\" to swim across.\n").lower()
+    if choice2 == "wait":
+        choice3 = input("You arrive to the island unharmed. There is a house with 3 doors. One red, one yellow, and one blue. Which color do you choose?\n").lower()
+        if choice3 == "blue":
+            print("You enter a room of beasts. Game over.")
+        elif choice3 == "red":
+            print("It's a room full of fire. Game over.")
+        elif choice3 == "yellow":
+            print("You found the treasure! You win!")
+        else:
+            print("You chose a door that doesn't exist. Game over.")
+    elif choice2 == "swim":
+        print("You get attacked by an angry trout. Game over.")
     else:
-      print("You chose a door that doesn't exist. Game over.")
-  elif choice2 == "swim":
-    print("You get attacked by an angry trout. Game over.")
-  else:
-    print("That's not an option. Game over.")
+        print("That's not an option. Game over.")
 elif choice1 == "right":
   print("You fell into a hole. Game over.")
 else:
